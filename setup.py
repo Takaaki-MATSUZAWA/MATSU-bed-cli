d051f838aa8bd085eb255f90c954744e7ee270f7
Takaaki-MATSUZAWA <matsuzawa@furo.org>
2018-05-05 22:38:17 +0900

    Fix verison in setup.py close #4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="MATSU-bed-cli",
-    version="0.1.0",
+    version="0.2.0",
     description="MATSU-bed offline development tool",
     url='https://github.com/hardtail0112/MATSU-bed-cli',
     author='TAKAKI MATSUZAWA',
