bdfce41191abece0821a580741fae0089d906e64
Takaaki-MATSUZAWA <matsuzawa@furo.org>
2022-08-18 16:38:30 +0900

    python3に合わせて少し修正

--- matsubed/matsubed.py
+++ matsubed/matsubed.py
@@ -77,7 +77,7 @@ def get_library_list():
 def get_drives():
     drives = []
     bitmask = windll.kernel32.GetLogicalDrives()
-    for letter in string.uppercase:
+    for letter in string.ascii_uppercase:
         if bitmask & 1:
             drives.append(letter)
         bitmask >>= 1
